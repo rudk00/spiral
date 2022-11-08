ac263f9bfddd51171948ee857d467a46fb0cd7c3
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-08 12:50:41 +0300

    Spiral array

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,35 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int [][] res = new int[rows][columns];
+        int num = 1;
+        int max = rows * columns;
+
+        int rmin = 0, rmax = rows - 1;
+        int cmin = 0, cmax = columns - 1;
+
+        while (num <= max) {
+
+            for (int i = cmin; num <= max && i <= cmax; i++) {
+                res[rmin][i] = num++;
+            }
+            rmin++;
+
+            for (int i = rmin; num <= max && i <= rmax; i++) {
+                res[i][cmax] = num++;
+            }
+            cmax--;
+
+            for (int i = cmax; num <= max && i >= cmin; i--) {
+                res[rmax][i] = num++;
+            }
+            rmax--;
+
+            for (int i = rmax; num <= max && i >= rmin; i--) {
+                res[i][cmin] = num++;
+            }
+            cmin++;
+        }
+        return res;
     }
 }
